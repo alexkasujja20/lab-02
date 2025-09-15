44546a841ea237cbfbafc7315877a39ba286de82
Alex Kasujja <C00315323@setu.ie>
2025-09-15 22:45:23 +0000

    modified

--- example2.py
+++ example2.py
@@ -1,3 +1,8 @@
 with open('sample.txt', 'r') as f:
     for line in f:
         print(line.strip())
+
+#modifying this code
+with open('sample.txt', 'r') as f:
+    for i, line in enumerate(f, start=1): #to start numbering from 1
+        print(f"{i}: {line.strip()}")
