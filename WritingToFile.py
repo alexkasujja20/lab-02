5563cef8239d6a0aeb8dc795fb0be784688328fb
Alex Kasujja <C00315323@setu.ie>
2025-09-19 10:05:23 +0100

    writing to file

--- WritingToFile.py
+++ WritingToFile.py
@@ -1,1 +1,6 @@
+#code to that copies files form one file to another
+with open("sample.txt|", "r") as source_file, open("copy.txt, "w") as dest_file:
+    for line in source_file:
+       new_File = dest_file.write(line)
 
+print(new_File)
